539853f76e9eec69c7d3916a2e4459263f474c84
Yan Barros <yanbarros98@gmail.com>
2022-02-23 15:27:43 -0300

    petweet textarea insert

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Routes/Home";
+import Profile from "./Routes/Profile";
 
 function App() {
-	return;
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/profile" element={<Profile />}></Route>
+			</Routes>
+		</>
+	);
 }
 
 export default App;
